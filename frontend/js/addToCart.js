e060b1fcee846513a53fbeb0d92b378fbe417c08
emhofer <p.emhofer@gmail.com>
2022-12-15 10:27:46 +0100

    worked on cart

--- frontend/js/addToCart.js
+++ frontend/js/addToCart.js
@@ -6,6 +6,14 @@ const addToCart = (item) => {
     localStorage.setItem("cart", JSON.stringify([item]));
     return;
   } else if (Array.isArray(currentCart)) {
+    for (let i = 0; i < currentCart.length; i++) {
+      if (currentCart[i].id === item.id) {
+        currentCart[i].quantity += 1;
+        localStorage.setItem("cart", JSON.stringify(currentCart));
+        return;
+      }
+    }
+    item.quantity = 1;
     currentCart.push(item);
     localStorage.setItem("cart", JSON.stringify(currentCart));
   }
