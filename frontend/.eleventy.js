f26a2d9244ed784753b126f95806b3d6af674529
emhofer <p.emhofer@gmail.com>
2022-12-26 11:01:40 +0100

    started adding 11ty

---
+++ frontend/.eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function (eleventyConfig) {
+  // Output directory: _site
+
+
+  // Copy `css/` to `_site/css/`
+  // Keeps the same directory structure.
+  eleventyConfig.addPassthroughCopy("css/");
+
+};
