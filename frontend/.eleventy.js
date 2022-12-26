3a3ca72d34b212c39a5becaf4dcce4d0589265d3
emhofer <p.emhofer@gmail.com>
2022-12-26 11:05:39 +0100

    added jpg passthrough

--- frontend/.eleventy.js
+++ frontend/.eleventy.js
@@ -1,9 +1,11 @@
 module.exports = function (eleventyConfig) {
   // Output directory: _site
 
-
   // Copy `css/` to `_site/css/`
   // Keeps the same directory structure.
   eleventyConfig.addPassthroughCopy("css/");
 
+  // Copy any .jpg file to `_site`, via Glob pattern
+  // Keeps the same directory structure.
+  eleventyConfig.addPassthroughCopy("**/*.jpg");
 };
